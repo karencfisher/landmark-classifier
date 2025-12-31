fc5b4d4e5c48eeec393711777abdb0d4c5b80241
karencfisher <karencfisher@hotmail.com>
2025-12-30 19:38:36 -0700

    moving on to transfer example

--- src/transfer.py
+++ src/transfer.py
@@ -21,15 +21,21 @@ def get_model_transfer_learning(model_name="resnet18", n_classes=50):
     # Freeze all parameters in the model
     # HINT: loop over all parameters. If "param" is one parameter,
     # "param.requires_grad = False" freezes it
-    # YOUR CODE HERE
+    for param in model_transfer.parameters():
+        param.requires_grad = False
 
     # Add the linear layer at the end with the appropriate number of classes
     # 1. get numbers of features extracted by the backbone
-    num_ftrs  = # YOUR CODE HERE
+    num_ftrs  = model_transfer.fc.in_features
 
     # 2. Create a new linear layer with the appropriate number of inputs and
     #    outputs
-    model_transfer.fc  = # YOUR CODE HERE
+    model_transfer.fc  = nn.Sequential(
+        nn.Linear(num_ftrs, 128),
+        nn.ReLU(),
+        nn.Dropout(0.2),
+        nn.Linear(128, n_classes)
+    )
 
     return model_transfer
 
@@ -42,7 +48,7 @@ import pytest
 
 @pytest.fixture(scope="session")
 def data_loaders():
-    from ..utils.data import get_data_loaders
+    from .data import get_data_loaders
 
     return get_data_loaders(batch_size=2)
 
